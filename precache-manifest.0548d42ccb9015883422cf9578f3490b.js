b47a5dac30ca75432e9d1d4044d29d8942c7fc55
Nicholas Tai <ntai@berkeley.edu>
2020-03-03 19:31:26 -0800

    Updates

---
+++ precache-manifest.0548d42ccb9015883422cf9578f3490b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d23702a7b764d5c5ac79e6d5c26e2195",
+    "url": "/nicholas/index.html"
+  },
+  {
+    "revision": "56285617af52eb82efa4",
+    "url": "/nicholas/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f1b87deeb00761ab4cc5",
+    "url": "/nicholas/static/js/2.1d1c72a0.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/nicholas/static/js/2.1d1c72a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56285617af52eb82efa4",
+    "url": "/nicholas/static/js/main.c124bce0.chunk.js"
+  },
+  {
+    "revision": "828819886be8506ffc24",
+    "url": "/nicholas/static/js/runtime-main.e77916a1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nicholas/static/media/logo.5d5d9eef.svg"
+  }
+]);
